258a505a599e28aa004dda99374f02cc42792c93
davisrobertson <darobertson@packer.edu>
2018-04-11 00:01:07 -0400

    added djt easter egg

--- docs/Input/script.js
+++ docs/Input/script.js
@@ -8,5 +8,10 @@ var val2 = document.getElementById("input2").value;
 val1 = parseInt(val1);
 val2 = parseInt(val2);
 
+
+if (val1 == 123 && val2 == 321) {
+	scriptable.innerHTML = "I will build a great wall. And nobody builds walls better than me, believe me. And I'll build them very inexpensively. I will build a great, great wall on our southern border, and I will make Mexico pay for that wall. Mark my words. -Donald J. Trump"
+}else {
 scriptable.innerHTML = val1 + val2;
+}
 }
