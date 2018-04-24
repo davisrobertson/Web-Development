00377c7c31a0375cb6ec6a85657c4581e33a9cd7
davisrobertson <darobertson@packer.edu>
2018-04-23 23:03:04 -0400

    Finished cipher and started decrypt part

--- docs/CaesarCipher/script.js
+++ docs/CaesarCipher/script.js
@@ -1,5 +1,6 @@
 document.getElementById("buttonEncode").addEventListener("click", encode);
 document.getElementById("buttonDecode").addEventListener("click", decode);
+document.getElementById("buttonDecrypt").addEventListener("click", decrypt)
 var outPut = document.getElementById("output");
 
 
@@ -48,5 +49,33 @@ function decode() {
     //console.log(PrintNew);
     
     outPut.innerHTML += PrintNew;
+}
+}
+
+function decrypt() {
+    var decryptKey = 0;
+    var input = document.getElementById("inputMessage").value;
+  //console.log("Button Pressed");
+    //console.log(input);
+    var length = input.length;
+
+        outPut.innerHTML = ("");
+
+
+
+    for (var i = 0; i < length; i++) {
+    	        for(var x = 0; x < 65536; x++) {
+    var charCode = input.charCodeAt(i);
+    //console.log("i=" + i);
+    //console.log("charCode=" + charCode);
+    var letter = charCode + decryptKey;
+    //console.log("letter=" + letter);
+
+    var PrintNew = (String.fromCharCode(letter));
+    //console.log(PrintNew);
+    
+    outPut.innerHTML += PrintNew;
+    decryptKey++;
+}
 }
 }
